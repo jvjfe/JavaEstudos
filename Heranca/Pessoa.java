9411c839cb99a2d3b7910bd85752c086c65dd50e
Jvjfe <82617361+jvjfe@users.noreply.github.com>
2024-10-22 19:50:12 -0300

    Create Pessoa.java

---
+++ Heranca/Pessoa.java
@@ -0,0 +1,16 @@
+
+package heranca;
+
+/**
+ *
+ * @author 2Ds_tec
+ */
+public class Pessoa {
+    String nome;
+    int idade;
+    
+    public Pessoa(){
+        this.nome = "";
+        this.idade = 0;
+    }
+}
